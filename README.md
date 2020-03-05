4a18c123ca8817fbc4f673c8c0c9c7b71d4684e8
rtraborn <rtraborn@asu.edu>
2020-03-04 19:20:14 -0500

    First commit; established this repo.

---
+++ README.md
@@ -0,0 +1,3 @@
+## GoSTRIPES repo for Chromochloris zofingiensis (annotation v5.2.3.2)
+## please see the STRIPES/tsr directory for annotated TSRs
+
---
+++ STRIPES/launch_job_Czo2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#PBS -N CzSTRIPE_job_Czo2
+#PBS -k o
+#PBS -l nodes=1:ppn=16,vmem=64gb
+#PBS -l walltime=8:00:00
+
+myDir=/N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Czo/STRIPES
+sampleFolder=Czo2
+
+module load singularity
+
+source /N/dc2/scratch/rtraborn/myDevel/GoSTRIPES/bin/xworkStripes -b /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Czo/STRIPES -i /N/dc2/scratch/rtraborn/STRIPEseq_files/GoSTRIPES_Czo/STRIPES/gostripes.simg
+
+echo "Launching job"
+
+cd $myDir
+
+cd $sampleFolder
+
+$rws make -n
+
+$rws make
+
+echo "Job complete"
